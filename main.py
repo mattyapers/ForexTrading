1b04893e0f16138be1728f13e98307e9ee029163
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-10-26 21:35:40 +0800

    test

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from infrastructure.instrument_collection import instrumentCollection
 if __name__ == '__main__':
     api = OandaApi()
 
-    # instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
+    instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
     instrumentCollection.LoadInstruments("./data")
     instrumentCollection.PrintInstruments()
