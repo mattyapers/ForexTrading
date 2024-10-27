df8fb1260c3e1d5ced9f13d228183157fa7cd74d
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-10-27 23:51:59 +0800

    changing the files

--- exploration/plotting.py
+++ exploration/plotting.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import plotly.graph_objects as go
+import pandas as pd
 
 class CandlePlot:
 
@@ -8,17 +9,17 @@ class CandlePlot:
         self.create_candle_fig()
 
     def add_timestr(self):
-        self.df['sTime'] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M") for x in self.df.time]
+        self.df_plot['sTime'] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M") for x in self.df_plot.time]
 
     def create_candle_fig(self):
         self.add_timestr()
         self.fig = go.Figure()
         self.fig.add_trace(go.Candlestick(
             x=self.df_plot['sTime'],
-            open=self.df_plot.mid_o,
-            high=self.df_plot.mid_h,
-            low=self.df_plot.mid_l,
-            close=self.df_plot.mid_c,
+            open=self.df_plot['mid_o'],
+            high=self.df_plot['mid_h'],
+            low=self.df_plot['mid_l'],
+            close=self.df_plot['mid_c'],
             line=dict(width=1), opacity=1,
             increasing_fillcolor="#24A06B",
             decreasing_fillcolor="#CC2E3C",
@@ -27,25 +28,17 @@ class CandlePlot:
         ))
 
     def update_layout(self, width, height, nticks):
-        self.fig.update_yaxes(
-        gridcolor="#1f292f"
-        )
-
-        self.fig.update_xaxes(
-        gridcolor="#1f292f",
-        rangeslider=dict(visible=False),
-        nticks=nticks
-        )
-
+        self.fig.update_yaxes(gridcolor="#1f292f")
+        self.fig.update_xaxes(gridcolor="#1f292f", rangeslider=dict(visible=False), nticks=nticks)
         self.fig.update_layout(
-        width=width,
-        height=height,
-        margin=dict(l=10, r=10, b=10, t=10),
-        paper_bgcolor="#2c303c",
-        plot_bgcolor="#2c303c",
-        font=dict(size=8, color="#e1e1e1")
+            width=width,
+            height=height,
+            margin=dict(l=10, r=10, b=10, t=10),
+            paper_bgcolor="#2c303c",
+            plot_bgcolor="#2c303c",
+            font=dict(size=8, color="#e1e1e1")
         )
-        
+
     def show_plot(self, width=900, height=400, nticks=5):
         self.update_layout(width, height, nticks)
         self.fig.show()
