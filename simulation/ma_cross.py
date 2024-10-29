38bb8858cdc6b8ef8ab2be9200eefce8707a38e2
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-10-29 18:27:30 +0800

    create ma cross script

---
+++ simulation/ma_cross.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from infrastructure.instrument_collection import instrumentCollection as ic
+
+BUY = 1
+SELL = -1
+NONE = 0
+get_ma_col = lambda x: f"MA_{x}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
