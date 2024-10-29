c17a6c8d9dc2e9148be1f218607e18fa1ed3271b
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-10-29 18:59:23 +0800

    testing main.py

--- simulation/ma_cross.py
+++ simulation/ma_cross.py
@@ -11,23 +11,14 @@ get_ma_col = lambda x: f"MA_{x}"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def run_ma_sim(curr_list=["EUR", "USD"],
+               granularity=["H1"],
+               ma_long=[20,40,80],
+               ma_short=[10,20]):
+    ic.LoadInstruments("./data")
+    for g in granularity:
+        for p1 in curr_list:
+            for p2 in curr:
+                pair = f"{p1}_{p2}"
+                if pair in ic.instruments_dict.keys():
+                    print(pair)
