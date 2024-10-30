0e32b2086fe382c65e1867cd13be9bb30bf29950
Matthew Yap <90490472+mattyapers@users.noreply.github.com>
2024-10-30 22:56:50 +0800

    reupdate

--- simulation/ma_cross.py
+++ simulation/ma_cross.py
@@ -6,8 +6,8 @@ class MAResult:
     def __init__(self, df_trades, pairname, ma_l, ma_s, granularity):
         self.pairname = pairname
         self.df_trades = df_trades
-        self.ma_l = ma_l
         self.ma_s = ma_s
+        self.ma_l = ma_l
         self.granularity = granularity
         self.result = self.result_ob()
 
@@ -15,29 +15,24 @@ class MAResult:
         return str(self.result)
 
     def result_ob(self):
-        # Guard against empty df_trades to avoid calculation errors
-        num_trades = self.df_trades.shape[0]
-        total_gain = int(self.df_trades['GAIN'].sum())
-        mean_gain = int(self.df_trades['GAIN'].mean()) if num_trades > 0 else 0
-        min_gain = int(self.df_trades['GAIN'].min()) if num_trades > 0 else 0
-        max_gain = int(self.df_trades['GAIN'].max()) if num_trades > 0 else 0
-
         return dict(
-            pair=self.pairname,
-            num_trades=num_trades,
-            total_gain=total_gain,
-            mean_gain=mean_gain,
-            min_gain=min_gain,
-            max_gain=max_gain,
-            ma_l=self.ma_l,
-            ma_s=self.ma_s,
-            granularity=self.granularity
+            pair = self.pairname,
+            num_trades = self.df_trades.shape[0],
+            total_gain = int(self.df_trades.GAIN.sum()),
+            mean_gain = int(self.df_trades.GAIN.mean()),
+            min_gain = int(self.df_trades.GAIN.min()),
+            max_gain = int(self.df_trades.GAIN.max()),
+            ma_l = self.ma_l,
+            ma_s = self.ma_s,
+            cross = f"{self.ma_s}_{self.ma_l}",
+            granularity = self.granularity
         )
 
 BUY = 1
 SELL = -1
 NONE = 0
 get_ma_col = lambda x: f"MA_{x}"
+add_cross = lambda x: f"{x.ma_s}_{x.ma_l}"
 
 def is_trade(row):
     if row.DELTA >= 0 and row.DELTA_PREV < 0:
@@ -49,40 +44,50 @@ def is_trade(row):
 def load_price_data(pair, granularity, ma_list):
     df = pd.read_pickle(f"./data/{pair}_{granularity}.pkl")
     for ma in ma_list:
-        df[get_ma_col(ma)] = df['mid_c'].rolling(window=ma).mean()
+        df[get_ma_col(ma)] = df.mid_c.rolling(window=ma).mean()
     df.dropna(inplace=True)
     df.reset_index(drop=True, inplace=True)
     return df
 
 def get_trades(df_analysis, instrument, granularity):
-    df_trades = df_analysis[df_analysis['TRADE'] != NONE].copy()
-    df_trades['DIFF'] = df_trades['mid_c'].diff().shift(-1).fillna(0)
-    df_trades['GAIN'] = (df_trades['DIFF'] / instrument.pipLocation) * df_trades['TRADE']
-    df_trades['granularity'] = granularity
-    df_trades['pair'] = instrument.name
-    df_trades['GAIN_C'] = df_trades['GAIN'].cumsum()
+    df_trades = df_analysis[df_analysis.TRADE != NONE].copy()
+    df_trades["DIFF"] = df_trades.mid_c.diff().shift(-1)
+    df_trades.fillna(0, inplace=True)
+    df_trades["GAIN"] = df_trades.DIFF / instrument.pipLocation
+    df_trades["GAIN"] = df_trades["GAIN"] * df_trades["TRADE"]
+    df_trades["granularity"] = granularity
+    df_trades["pair"] = instrument.name
+    df_trades["GAIN_C"] = df_trades["GAIN"].cumsum()
     return df_trades
+
 
 def assess_pair(price_data, ma_l, ma_s, instrument, granularity):
     df_analysis = price_data.copy()
-    df_analysis['DELTA'] = df_analysis[ma_s] - df_analysis[ma_l]
-    df_analysis['DELTA_PREV'] = df_analysis['DELTA'].shift(1)
-    df_analysis['TRADE'] = df_analysis.apply(is_trade, axis=1)
+    df_analysis["DELTA"] = df_analysis[ma_s] - df_analysis[ma_l]
+    df_analysis["DELTA_PREV"] = df_analysis["DELTA"].shift(1)
+    df_analysis["TRADE"] = df_analysis.apply(is_trade, axis=1)
     df_trades = get_trades(df_analysis, instrument, granularity)
-    df_trades['ma_l'] = ma_l
-    df_trades['ma_s'] = ma_s
-    
-    # Creating MAResult object with the relevant information
+    df_trades["ma_l"] = ma_l
+    df_trades["ma_s"] = ma_s
+    df_trades["cross"] = df_trades.apply(add_cross, axis=1)
     return MAResult(
-        df_trades=df_trades,
-        pairname=instrument.name,
-        ma_l=ma_l,
-        ma_s=ma_s,
-        granularity=granularity
+        df_trades,
+        instrument.name,
+        ma_l,
+        ma_s,
+        granularity
     )
 
 def append_df_to_file(df, filename):
-    pass
+
+    if os.path.isfile(filename):
+        fd = pd.read_pickle(filename)
+        df = pd.concat([fd, df])
+
+    df.reset_index(inplace=True, drop=True)
+    df.to_pickle(filename)
+    print(filename, df.shape)
+    print(df.tail(2))
 
 def get_fullname(filepath, filename):
     return f"{filepath}/{filename}.pkl"
@@ -99,23 +104,22 @@ def process_trades(results_list, filename):
 def process_results(results_list, filepath):
     process_macro(results_list, get_fullname(filepath, "ma_res"))
     process_trades(results_list, get_fullname(filepath, "ma_trades"))
-    # Convert list of results to DataFrame for summary analysis
-    # rl = [x.result for x in results_list]
-    # df = pd.DataFrame.from_dict(rl)
-    # print(df)
-    # print(results_list[0].df_trades.head(2))
+
 
 def analyse_pair(instrument, granularity, ma_long, ma_short, filepath):
+
     ma_list = set(ma_long + ma_short)
     pair = instrument.name
+
     price_data = load_price_data(pair, granularity, ma_list)
+
     results_list = []
+
     for ma_l in ma_long:
         for ma_s in ma_short:
             if ma_l <= ma_s:
                 continue
 
-            # Assess moving average pair and add result to list
             ma_result = assess_pair(
                 price_data,
                 get_ma_col(ma_l),
@@ -123,15 +127,16 @@ def analyse_pair(instrument, granularity, ma_long, ma_short, filepath):
                 instrument,
                 granularity
             )
-            print(ma_result)
+            #print(ma_result)
             results_list.append(ma_result)
     process_results(results_list, filepath)
 
+
 def run_ma_sim(curr_list=["EUR", "USD"],
-               granularity=["H1", "H4"],
-               ma_long=[20, 40, 80],
-               ma_short=[10, 20],
-               filepath="/.data"):
+                granularity=["H1", "H4"],
+                ma_long=[20,40,80,120,150],
+                ma_short=[10,20,30,40],
+                filepath="./data"):
     ic.LoadInstruments("./data")
     for g in granularity:
         for p1 in curr_list:
@@ -139,3 +144,9 @@ def run_ma_sim(curr_list=["EUR", "USD"],
                 pair = f"{p1}_{p2}"
                 if pair in ic.instruments_dict.keys():
                     analyse_pair(ic.instruments_dict[pair], g, ma_long, ma_short, filepath)
+
+
+
+
+
+
